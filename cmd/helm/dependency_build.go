helm/helm

feat(helm): add 'helm dependency' commands
This also refactors significant portions of the CLI, moving much of the
shared code into a library.

Also in this release, a testing repository server has been added.

cmd/helm/dependency_build.go
@@ -1,0 +1,85 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"io"
+
+	"github.com/spf13/cobra"
+
+	"k8s.io/helm/cmd/helm/downloader"
+	"k8s.io/helm/cmd/helm/helmpath"
+)
+
+const dependencyBuildDesc = `
+Build out the charts/ directory from the requirements.lock file.
+
+Build is used to reconstruct a chart's dependencies to the state specified in
+the lock file. This will not re-negotiate dependencies, as 'helm dependency update'
+does.
+
+If no lock file is found, 'helm dependency build' will mirror the behavior
+of 'helm dependency update'.
+`
+
+type dependencyBuildCmd struct {
+	out       io.Writer
+	chartpath string
+	verify    bool
+	keyring   string
+	helmhome  helmpath.HelmHome
+}
+
+func newDependencyBuildCmd(out io.Writer) *cobra.Command {
+	dbc := &dependencyBuildCmd{
+		out: out,
+	}
+
+	cmd := &cobra.Command{
+		Use:   "build [flags] CHART",
+		Short: "rebuild the charts/ directory based on the requirements.lock file",
+		Long:  dependencyBuildDesc,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			dbc.helmhome = helmpath.HelmHome(homePath())
+			dbc.chartpath = "."
+
+			if len(args) > 0 {
+				dbc.chartpath = args[0]
+			}
+			return dbc.run()
+		},
+	}
+
+	f := cmd.Flags()
+	f.BoolVar(&dbc.verify, "verify", false, "Verify the packages against signatures.")
+	f.StringVar(&dbc.keyring, "keyring", defaultKeyring(), "The keyring containing public keys.")
+
+	return cmd
+}
+
+func (d *dependencyBuildCmd) run() error {
+	man := &downloader.Manager{
+		Out:       d.out,
+		ChartPath: d.chartpath,
+		HelmHome:  helmpath.HelmHome(d.helmhome),
+		Keyring:   d.keyring,
+	}
+	if d.verify {
+		man.Verify = downloader.VerifyIfPossible
+	}
+
+	return man.Build()
+}

cmd/helm/dependency_update.go
@@ -16,21 +16,12 @@ limitations under the License.
 package main
 
 import (
-	"errors"
-	"fmt"
 	"io"
-	"io/ioutil"
-	"net/url"
-	"os"
 	"path/filepath"
-	"strings"
 
-	"github.com/ghodss/yaml"
 	"github.com/spf13/cobra"
-
-	"k8s.io/helm/cmd/helm/resolver"
-	"k8s.io/helm/pkg/chartutil"
-	"k8s.io/helm/pkg/repo"
+	"k8s.io/helm/cmd/helm/downloader"
+	"k8s.io/helm/cmd/helm/helmpath"
 )
 
 const dependencyUpDesc = `
@@ -38,15 +29,16 @@ Update the on-disk dependencies to mirror the requirements.yaml file.
 
 This command verifies that the required charts, as expressed in 'requirements.yaml',
 are present in 'charts/' and are at an acceptable version.
+
+On successful update, this will generate a lock file that can be used to
+rebuild the requirements to an exact version.
 `
 
 // dependencyUpdateCmd describes a 'helm dependency update'
 type dependencyUpdateCmd struct {
 	out       io.Writer
 	chartpath string
-	repoFile  string
-	repopath  string
-	helmhome  string
+	helmhome  helmpath.HelmHome
 	verify    bool
 	keyring   string
 }
@@ -74,16 +66,14 @@ func newDependencyUpdateCmd(out io.Writer) *cobra.Command {
 				return err
 			}
 
-			duc.helmhome = homePath()
-			duc.repoFile = repositoriesFile()
-			duc.repopath = repositoryDirectory()
+			duc.helmhome = helmpath.HelmHome(homePath())
 
 			return duc.run()
 		},
 	}
 
 	f := cmd.Flags()
-	f.BoolVar(&duc.verify, "verify", false, "Verify the package against its signature.")
+	f.BoolVar(&duc.verify, "verify", false, "Verify the packages against signatures.")
 	f.StringVar(&duc.keyring, "keyring", defaultKeyring(), "The keyring containing public keys.")
 
 	return cmd
@@ -91,197 +81,14 @@ func newDependencyUpdateCmd(out io.Writer) *cobra.Command {
 
 // run runs the full dependency update process.
 func (d *dependencyUpdateCmd) run() error {
-	if fi, err := os.Stat(d.chartpath); err != nil {
-		return fmt.Errorf("could not find %s: %s", d.chartpath, err)
-	} else if !fi.IsDir() {
-		return errors.New("only unpacked charts can be updated")
+	man := &downloader.Manager{
+		Out:       d.out,
+		ChartPath: d.chartpath,
+		HelmHome:  d.helmhome,
+		Keyring:   d.keyring,
 	}
-	c, err := chartutil.LoadDir(d.chartpath)
-	if err != nil {
-		return err
+	if d.verify {
+		man.Verify = downloader.VerifyIfPossible
 	}
-
-	req, err := chartutil.LoadRequirements(c)
-	if err != nil {
-		if err == chartutil.ErrRequirementsNotFound {
-			fmt.Fprintf(d.out, "No requirements found in %s/charts.\n", d.chartpath)
-			return nil
-		}
-		return err
-	}
-
-	// For each repo in the file, update the cached copy of that repo
-	if _, err := d.updateRepos(req.Dependencies); err != nil {
-		return err
-	}
-
-	// Now we need to find out which version of a chart best satisfies the
-	// requirements the requirements.yaml
-	lock, err := d.resolve(req)
-	if err != nil {
-		return err
-	}
-
-	// Now we need to fetch every package here into charts/
-	if err := d.downloadAll(lock.Dependencies); err != nil {
-		return err
-	}
-
-	// Finally, we need to write the lockfile.
-	return writeLock(d.chartpath, lock)
-}
-
-// resolve takes a list of requirements and translates them into an exact version to download.
-//
-// This returns a lock file, which has all of the requirements normalized to a specific version.
-func (d *dependencyUpdateCmd) resolve(req *chartutil.Requirements) (*chartutil.RequirementsLock, error) {
-	res := resolver.New(d.chartpath, d.helmhome)
-	return res.Resolve(req)
-}
-
-// downloadAll takes a list of dependencies and downloads them into charts/
-func (d *dependencyUpdateCmd) downloadAll(deps []*chartutil.Dependency) error {
-	repos, err := loadChartRepositories(d.repopath)
-	if err != nil {
-		return err
-	}
-
-	fmt.Fprintf(d.out, "Saving %d charts\n", len(deps))
-	for _, dep := range deps {
-		fmt.Fprintf(d.out, "Downloading %s from repo %s\n", dep.Name, dep.Repository)
-
-		target := fmt.Sprintf("%s-%s", dep.Name, dep.Version)
-		churl, err := findChartURL(target, dep.Repository, repos)
-		if err != nil {
-			fmt.Fprintf(d.out, "WARNING: %s (skipped)", err)
-			continue
-		}
-
-		dest := filepath.Join(d.chartpath, "charts", target+".tgz")
-		data, err := downloadChart(churl, d.verify, d.keyring)
-		if err != nil {
-			fmt.Fprintf(d.out, "WARNING: Could not download %s: %s (skipped)", churl, err)
-			continue
-		}
-		if err := ioutil.WriteFile(dest, data.Bytes(), 0655); err != nil {
-			fmt.Fprintf(d.out, "WARNING: %s (skipped)", err)
-			continue
-		}
-	}
-	return nil
-}
-
-// updateRepos updates all of the local repos to their latest.
-//
-// If one of the dependencies present is not in the cached repos, this will error out. The
-// consequence of that is that every repository referenced in a requirements.yaml file
-// must also be added with 'helm repo add'.
-func (d *dependencyUpdateCmd) updateRepos(deps []*chartutil.Dependency) (*repo.RepoFile, error) {
-	// TODO: In the future, we could make it so that only the repositories that
-	// are used by this chart are updated. As it is, we're mainly doing some sanity
-	// checking here.
-	rf, err := repo.LoadRepositoriesFile(d.repoFile)
-	if err != nil {
-		return rf, err
-	}
-	repos := rf.Repositories
-
-	// Verify that all repositories referenced in the deps are actually known
-	// by Helm.
-	missing := []string{}
-	for _, dd := range deps {
-		found := false
-		if dd.Repository == "" {
-			found = true
-		} else {
-			for _, repo := range repos {
-				if urlsAreEqual(repo, dd.Repository) {
-					found = true
-				}
-			}
-		}
-		if !found {
-			missing = append(missing, dd.Repository)
-		}
-	}
-
-	if len(missing) > 0 {
-		return rf, fmt.Errorf("no repository definition for %s. Try 'helm repo add'", strings.Join(missing, ", "))
-	}
-
-	if len(repos) > 0 {
-		// This prints errors straight to out.
-		updateCharts(repos, flagDebug, d.out)
-	}
-	return rf, nil
-}
-
-// urlsAreEqual normalizes two URLs and then compares for equality.
-func urlsAreEqual(a, b string) bool {
-	au, err := url.Parse(a)
-	if err != nil {
-		return a == b
-	}
-	bu, err := url.Parse(b)
-	if err != nil {
-		return false
-	}
-	return au.String() == bu.String()
-}
-
-// findChartURL searches the cache of repo data for a chart that has the name and the repourl specified.
-//
-// In this current version, name is of the form 'foo-1.2.3'. This will change when
-// the repository index stucture changes.
-func findChartURL(name, repourl string, repos map[string]*repo.ChartRepository) (string, error) {
-	for _, cr := range repos {
-		if urlsAreEqual(repourl, cr.URL) {
-			for ename, entry := range cr.IndexFile.Entries {
-				if ename == name {
-					return entry.URL, nil
-				}
-			}
-		}
-	}
-	return "", fmt.Errorf("chart %s not found in %s", name, repourl)
-}
-
-// loadChartRepositories reads the repositories.yaml, and then builds a map of
-// ChartRepositories.
-//
-// The key is the local name (which is only present in the repositories.yaml).
-func loadChartRepositories(repodir string) (map[string]*repo.ChartRepository, error) {
-	indices := map[string]*repo.ChartRepository{}
-	repoyaml := repositoriesFile()
-
-	// Load repositories.yaml file
-	rf, err := repo.LoadRepositoriesFile(repoyaml)
-	if err != nil {
-		return indices, fmt.Errorf("failed to load %s: %s", repoyaml, err)
-	}
-
-	// localName: chartRepo
-	for lname, url := range rf.Repositories {
-		index, err := repo.LoadIndexFile(cacheIndexFile(lname))
-		if err != nil {
-			return indices, err
-		}
-
-		cr := &repo.ChartRepository{
-			URL:       url,
-			IndexFile: index,
-		}
-		indices[lname] = cr
-	}
-	return indices, nil
-}
-
-// writeLock writes a lockfile to disk
-func writeLock(chartpath string, lock *chartutil.RequirementsLock) error {
-	data, err := yaml.Marshal(lock)
-	if err != nil {
-		return err
-	}
-	dest := filepath.Join(chartpath, "requirements.lock")
-	return ioutil.WriteFile(dest, data, 0755)
+	return man.Update()
 }

cmd/helm/install.go
@@ -32,6 +32,8 @@ import (
 	"github.com/ghodss/yaml"
 	"github.com/spf13/cobra"
 
+	"k8s.io/helm/cmd/helm/downloader"
+	"k8s.io/helm/cmd/helm/helmpath"
 	"k8s.io/helm/pkg/helm"
 	"k8s.io/helm/pkg/proto/hapi/release"
 	"k8s.io/helm/pkg/timeconv"
@@ -286,7 +288,7 @@ func locateChartPath(name string, verify bool, keyring string) (string, error) {
 			if fi.IsDir() {
 				return "", errors.New("cannot verify a directory")
 			}
-			if err := verifyChart(abs, keyring); err != nil {
+			if _, err := downloader.VerifyChart(abs, keyring); err != nil {
 				return "", err
 			}
 		}
@@ -306,7 +308,17 @@ func locateChartPath(name string, verify bool, keyring string) (string, error) {
 	if filepath.Ext(name) != ".tgz" {
 		name += ".tgz"
 	}
-	if err := downloadAndSaveChart(name, false, ".", verify, keyring); err == nil {
+
+	dl := downloader.ChartDownloader{
+		HelmHome: helmpath.HelmHome(homePath()),
+		Out:      os.Stdout,
+		Keyring:  keyring,
+	}
+	if verify {
+		dl.Verify = downloader.VerifyAlways
+	}
+
+	if _, err := dl.DownloadTo(name, "."); err == nil {
 		lname, err := filepath.Abs(filepath.Base(name))
 		if err != nil {
 			return lname, err

werf/3p-helm

cmd/helm/dependency_build.go
@@ -1,0 +1,85 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"io"
+
+	"github.com/spf13/cobra"
+
+	"k8s.io/helm/cmd/helm/downloader"
+	"k8s.io/helm/cmd/helm/helmpath"
+)
+
+const dependencyBuildDesc = `
+Build out the charts/ directory from the requirements.lock file.
+
+Build is used to reconstruct a chart's dependencies to the state specified in
+the lock file. This will not re-negotiate dependencies, as 'helm dependency update'
+does.
+
+If no lock file is found, 'helm dependency build' will mirror the behavior
+of 'helm dependency update'.
+`
+
+type dependencyBuildCmd struct {
+	out       io.Writer
+	chartpath string
+	verify    bool
+	keyring   string
+	helmhome  helmpath.Home
+}
+
+func newDependencyBuildCmd(out io.Writer) *cobra.Command {
+	dbc := &dependencyBuildCmd{
+		out: out,
+	}
+
+	cmd := &cobra.Command{
+		Use:   "build [flags] CHART",
+		Short: "rebuild the charts/ directory based on the requirements.lock file",
+		Long:  dependencyBuildDesc,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			dbc.helmhome = helmpath.Home(homePath())
+			dbc.chartpath = "."
+
+			if len(args) > 0 {
+				dbc.chartpath = args[0]
+			}
+			return dbc.run()
+		},
+	}
+
+	f := cmd.Flags()
+	f.BoolVar(&dbc.verify, "verify", false, "Verify the packages against signatures.")
+	f.StringVar(&dbc.keyring, "keyring", defaultKeyring(), "The keyring containing public keys.")
+
+	return cmd
+}
+
+func (d *dependencyBuildCmd) run() error {
+	man := &downloader.Manager{
+		Out:       d.out,
+		ChartPath: d.chartpath,
+		HelmHome:  d.helmhome,
+		Keyring:   d.keyring,
+	}
+	if d.verify {
+		man.Verify = downloader.VerifyIfPossible
+	}
+
+	return man.Build()
+}

cmd/helm/dependency_update.go
@@ -16,21 +16,12 @@ limitations under the License.
 package main
 
 import (
-	"errors"
-	"fmt"
 	"io"
-	"io/ioutil"
-	"net/url"
-	"os"
 	"path/filepath"
-	"strings"
 
-	"github.com/ghodss/yaml"
 	"github.com/spf13/cobra"
-
-	"k8s.io/helm/cmd/helm/resolver"
-	"k8s.io/helm/pkg/chartutil"
-	"k8s.io/helm/pkg/repo"
+	"k8s.io/helm/cmd/helm/downloader"
+	"k8s.io/helm/cmd/helm/helmpath"
 )
 
 const dependencyUpDesc = `
@@ -38,15 +29,16 @@ Update the on-disk dependencies to mirror the requirements.yaml file.
 
 This command verifies that the required charts, as expressed in 'requirements.yaml',
 are present in 'charts/' and are at an acceptable version.
+
+On successful update, this will generate a lock file that can be used to
+rebuild the requirements to an exact version.
 `
 
 // dependencyUpdateCmd describes a 'helm dependency update'
 type dependencyUpdateCmd struct {
 	out       io.Writer
 	chartpath string
-	repoFile  string
-	repopath  string
-	helmhome  string
+	helmhome  helmpath.Home
 	verify    bool
 	keyring   string
 }
@@ -74,16 +66,14 @@ func newDependencyUpdateCmd(out io.Writer) *cobra.Command {
 				return err
 			}
 
-			duc.helmhome = homePath()
-			duc.repoFile = repositoriesFile()
-			duc.repopath = repositoryDirectory()
+			duc.helmhome = helmpath.Home(homePath())
 
 			return duc.run()
 		},
 	}
 
 	f := cmd.Flags()
-	f.BoolVar(&duc.verify, "verify", false, "Verify the package against its signature.")
+	f.BoolVar(&duc.verify, "verify", false, "Verify the packages against signatures.")
 	f.StringVar(&duc.keyring, "keyring", defaultKeyring(), "The keyring containing public keys.")
 
 	return cmd
@@ -91,197 +81,14 @@ func newDependencyUpdateCmd(out io.Writer) *cobra.Command {
 
 // run runs the full dependency update process.
 func (d *dependencyUpdateCmd) run() error {
-	if fi, err := os.Stat(d.chartpath); err != nil {
-		return fmt.Errorf("could not find %s: %s", d.chartpath, err)
-	} else if !fi.IsDir() {
-		return errors.New("only unpacked charts can be updated")
+	man := &downloader.Manager{
+		Out:       d.out,
+		ChartPath: d.chartpath,
+		HelmHome:  d.helmhome,
+		Keyring:   d.keyring,
 	}
-	c, err := chartutil.LoadDir(d.chartpath)
-	if err != nil {
-		return err
+	if d.verify {
+		man.Verify = downloader.VerifyIfPossible
 	}
-
-	req, err := chartutil.LoadRequirements(c)
-	if err != nil {
-		if err == chartutil.ErrRequirementsNotFound {
-			fmt.Fprintf(d.out, "No requirements found in %s/charts.\n", d.chartpath)
-			return nil
-		}
-		return err
-	}
-
-	// For each repo in the file, update the cached copy of that repo
-	if _, err := d.updateRepos(req.Dependencies); err != nil {
-		return err
-	}
-
-	// Now we need to find out which version of a chart best satisfies the
-	// requirements the requirements.yaml
-	lock, err := d.resolve(req)
-	if err != nil {
-		return err
-	}
-
-	// Now we need to fetch every package here into charts/
-	if err := d.downloadAll(lock.Dependencies); err != nil {
-		return err
-	}
-
-	// Finally, we need to write the lockfile.
-	return writeLock(d.chartpath, lock)
-}
-
-// resolve takes a list of requirements and translates them into an exact version to download.
-//
-// This returns a lock file, which has all of the requirements normalized to a specific version.
-func (d *dependencyUpdateCmd) resolve(req *chartutil.Requirements) (*chartutil.RequirementsLock, error) {
-	res := resolver.New(d.chartpath, d.helmhome)
-	return res.Resolve(req)
-}
-
-// downloadAll takes a list of dependencies and downloads them into charts/
-func (d *dependencyUpdateCmd) downloadAll(deps []*chartutil.Dependency) error {
-	repos, err := loadChartRepositories(d.repopath)
-	if err != nil {
-		return err
-	}
-
-	fmt.Fprintf(d.out, "Saving %d charts\n", len(deps))
-	for _, dep := range deps {
-		fmt.Fprintf(d.out, "Downloading %s from repo %s\n", dep.Name, dep.Repository)
-
-		target := fmt.Sprintf("%s-%s", dep.Name, dep.Version)
-		churl, err := findChartURL(target, dep.Repository, repos)
-		if err != nil {
-			fmt.Fprintf(d.out, "WARNING: %s (skipped)", err)
-			continue
-		}
-
-		dest := filepath.Join(d.chartpath, "charts", target+".tgz")
-		data, err := downloadChart(churl, d.verify, d.keyring)
-		if err != nil {
-			fmt.Fprintf(d.out, "WARNING: Could not download %s: %s (skipped)", churl, err)
-			continue
-		}
-		if err := ioutil.WriteFile(dest, data.Bytes(), 0655); err != nil {
-			fmt.Fprintf(d.out, "WARNING: %s (skipped)", err)
-			continue
-		}
-	}
-	return nil
-}
-
-// updateRepos updates all of the local repos to their latest.
-//
-// If one of the dependencies present is not in the cached repos, this will error out. The
-// consequence of that is that every repository referenced in a requirements.yaml file
-// must also be added with 'helm repo add'.
-func (d *dependencyUpdateCmd) updateRepos(deps []*chartutil.Dependency) (*repo.RepoFile, error) {
-	// TODO: In the future, we could make it so that only the repositories that
-	// are used by this chart are updated. As it is, we're mainly doing some sanity
-	// checking here.
-	rf, err := repo.LoadRepositoriesFile(d.repoFile)
-	if err != nil {
-		return rf, err
-	}
-	repos := rf.Repositories
-
-	// Verify that all repositories referenced in the deps are actually known
-	// by Helm.
-	missing := []string{}
-	for _, dd := range deps {
-		found := false
-		if dd.Repository == "" {
-			found = true
-		} else {
-			for _, repo := range repos {
-				if urlsAreEqual(repo, dd.Repository) {
-					found = true
-				}
-			}
-		}
-		if !found {
-			missing = append(missing, dd.Repository)
-		}
-	}
-
-	if len(missing) > 0 {
-		return rf, fmt.Errorf("no repository definition for %s. Try 'helm repo add'", strings.Join(missing, ", "))
-	}
-
-	if len(repos) > 0 {
-		// This prints errors straight to out.
-		updateCharts(repos, flagDebug, d.out)
-	}
-	return rf, nil
-}
-
-// urlsAreEqual normalizes two URLs and then compares for equality.
-func urlsAreEqual(a, b string) bool {
-	au, err := url.Parse(a)
-	if err != nil {
-		return a == b
-	}
-	bu, err := url.Parse(b)
-	if err != nil {
-		return false
-	}
-	return au.String() == bu.String()
-}
-
-// findChartURL searches the cache of repo data for a chart that has the name and the repourl specified.
-//
-// In this current version, name is of the form 'foo-1.2.3'. This will change when
-// the repository index stucture changes.
-func findChartURL(name, repourl string, repos map[string]*repo.ChartRepository) (string, error) {
-	for _, cr := range repos {
-		if urlsAreEqual(repourl, cr.URL) {
-			for ename, entry := range cr.IndexFile.Entries {
-				if ename == name {
-					return entry.URL, nil
-				}
-			}
-		}
-	}
-	return "", fmt.Errorf("chart %s not found in %s", name, repourl)
-}
-
-// loadChartRepositories reads the repositories.yaml, and then builds a map of
-// ChartRepositories.
-//
-// The key is the local name (which is only present in the repositories.yaml).
-func loadChartRepositories(repodir string) (map[string]*repo.ChartRepository, error) {
-	indices := map[string]*repo.ChartRepository{}
-	repoyaml := repositoriesFile()
-
-	// Load repositories.yaml file
-	rf, err := repo.LoadRepositoriesFile(repoyaml)
-	if err != nil {
-		return indices, fmt.Errorf("failed to load %s: %s", repoyaml, err)
-	}
-
-	// localName: chartRepo
-	for lname, url := range rf.Repositories {
-		index, err := repo.LoadIndexFile(cacheIndexFile(lname))
-		if err != nil {
-			return indices, err
-		}
-
-		cr := &repo.ChartRepository{
-			URL:       url,
-			IndexFile: index,
-		}
-		indices[lname] = cr
-	}
-	return indices, nil
-}
-
-// writeLock writes a lockfile to disk
-func writeLock(chartpath string, lock *chartutil.RequirementsLock) error {
-	data, err := yaml.Marshal(lock)
-	if err != nil {
-		return err
-	}
-	dest := filepath.Join(chartpath, "requirements.lock")
-	return ioutil.WriteFile(dest, data, 0755)
+	return man.Update()
 }

cmd/helm/install.go
@@ -32,6 +32,8 @@ import (
 	"github.com/ghodss/yaml"
 	"github.com/spf13/cobra"
 
+	"k8s.io/helm/cmd/helm/downloader"
+	"k8s.io/helm/cmd/helm/helmpath"
 	"k8s.io/helm/pkg/helm"
 	"k8s.io/helm/pkg/proto/hapi/release"
 	"k8s.io/helm/pkg/timeconv"
@@ -286,7 +288,7 @@ func locateChartPath(name string, verify bool, keyring string) (string, error) {
 			if fi.IsDir() {
 				return "", errors.New("cannot verify a directory")
 			}
-			if err := verifyChart(abs, keyring); err != nil {
+			if _, err := downloader.VerifyChart(abs, keyring); err != nil {
 				return "", err
 			}
 		}
@@ -306,7 +308,17 @@ func locateChartPath(name string, verify bool, keyring string) (string, error) {
 	if filepath.Ext(name) != ".tgz" {
 		name += ".tgz"
 	}
-	if err := downloadAndSaveChart(name, false, ".", verify, keyring); err == nil {
+
+	dl := downloader.ChartDownloader{
+		HelmHome: helmpath.Home(homePath()),
+		Out:      os.Stdout,
+		Keyring:  keyring,
+	}
+	if verify {
+		dl.Verify = downloader.VerifyAlways
+	}
+
+	if _, err := dl.DownloadTo(name, "."); err == nil {
 		lname, err := filepath.Abs(filepath.Base(name))
 		if err != nil {
 			return lname, err
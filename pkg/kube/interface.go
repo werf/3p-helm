219e498b48472f6f797271df220f4ef193757e79
Timofey Kirillov <timofey.kirillov@flant.com>
2020-09-21 20:13:27 +0300

    feat(helm for werf): introduce wait option for resources deletion using ResourcesWaiter
    Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>

--- pkg/kube/interface.go
+++ pkg/kube/interface.go
@@ -23,6 +23,11 @@ import (
 	v1 "k8s.io/api/core/v1"
 )
 
+type DeleteOptions struct {
+	Wait        bool
+	WaitTimeout time.Duration
+}
+
 // Interface represents a client capable of communicating with the Kubernetes API.
 //
 // A KubernetesClient must be concurrency safe.
@@ -37,7 +42,7 @@ type Interface interface {
 	WaitWithJobs(resources ResourceList, timeout time.Duration) error
 
 	// Delete destroys one or more resources.
-	Delete(resources ResourceList) (*Result, []error)
+	Delete(resources ResourceList, opts DeleteOptions) (*Result, []error)
 
 	// WatchUntilReady watches the resources given and waits until it is ready.
 	//

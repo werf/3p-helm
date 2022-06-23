94c74ddf2bb82785c163d29668c6ee255734d24e
Ilya Lesikov <ilya@lesikov.com>
2022-06-23 13:07:07 +0300

    fix(external-deps): use Unstructured instead of builtin types
    Signed-off-by: Ilya Lesikov <ilya@lesikov.com>

--- pkg/phases/stages/externaldeps/external_dependency.go
+++ pkg/phases/stages/externaldeps/external_dependency.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/cli-runtime/pkg/resource"
 )
 
@@ -25,31 +25,20 @@ type ExternalDependency struct {
 	Info      *resource.Info
 }
 
-func (d *ExternalDependency) GenerateInfo(gvkBuilder GVKBuilder, scheme *runtime.Scheme, metaAccessor meta.MetadataAccessor) error {
+func (d *ExternalDependency) GenerateInfo(gvkBuilder GVKBuilder, metaAccessor meta.MetadataAccessor) error {
 	gvk, err := gvkBuilder.BuildFromResource(d.ResourceType)
 	if err != nil {
 		return fmt.Errorf("error building GroupVersionKind from resource type: %w", err)
 	}
 
-	object, err := scheme.New(*gvk)
-	if err != nil {
-		return fmt.Errorf("error creating new object from GroupVersionKind: %w", err)
-	}
+	object := unstructured.Unstructured{}
 
-	object.GetObjectKind().SetGroupVersionKind(*gvk)
-
-	if err := metaAccessor.SetName(object, d.ResourceName); err != nil {
-		return fmt.Errorf("error setting name for an object: %w", err)
-	}
-
-	if d.Namespace != "" {
-		if err := metaAccessor.SetNamespace(object, d.Namespace); err != nil {
-			return fmt.Errorf("error setting namespace for an object: %w", err)
-		}
-	}
+	object.SetGroupVersionKind(*gvk)
+	object.SetName(d.ResourceName)
+	object.SetNamespace(d.Namespace)
 
 	d.Info = &resource.Info{
-		Object:    object,
+		Object:    &object,
 		Name:      d.ResourceName,
 		Namespace: d.Namespace,
 	}

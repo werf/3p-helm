b2fc45bbcb8766b083e7f6efcfcd7b214869068c
Ilya Lesikov <ilya@lesikov.com>
2025-05-23 13:47:54 +0300

    refactor: get rid of useless debug logs
    Signed-off-by: Ilya Lesikov <ilya@lesikov.com>

--- pkg/chartutil/coalesce.go
+++ pkg/chartutil/coalesce.go
@@ -20,15 +20,12 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/mitchellh/copystructure"
 	"github.com/pkg/errors"
-	"sigs.k8s.io/yaml"
 
 	"github.com/werf/3p-helm/pkg/chart"
 	"github.com/werf/3p-helm/pkg/werf/secrets"
-	"github.com/werf/logboek"
 )
 
 func concatPrefix(a, b string) string {
@@ -331,39 +328,16 @@ func makeValues(chrt *chart.Chart, vals map[string]interface{}) (map[string]inte
 	return result, nil
 }
 
-func DebugPrintValues(ctx context.Context, name string, vals map[string]interface{}) {
-	data, err := yaml.Marshal(vals)
-	if err != nil {
-		logboek.Context(ctx).Debug().LogF("Unable to marshal %q values: %s\n", err)
-	} else {
-		logboek.Context(ctx).Debug().LogF("%q values:\n%s---\n", name, data)
-	}
-}
-
-func DebugSecretValues() bool {
-	return os.Getenv("WERF_DEBUG_SECRET_VALUES") == "1"
-}
-
 func MergeInternal(ctx context.Context, inputVals, serviceVals map[string]interface{}, decryptedSecretValues map[string]interface{}) (map[string]interface{}, error) {
 	vals := make(map[string]interface{})
 
-	DebugPrintValues(ctx, "service", serviceVals)
 	CoalesceTables(vals, serviceVals) // NOTE: service values will not be saved into the marshalled release
 
 	if decryptedSecretValues != nil {
-		if DebugSecretValues() {
-			DebugPrintValues(ctx, "secret", decryptedSecretValues)
-		}
 		CoalesceTables(vals, decryptedSecretValues)
 	}
 
-	DebugPrintValues(ctx, "input", inputVals)
 	CoalesceTables(vals, inputVals)
-
-	if DebugSecretValues() {
-		// Only print all values with secrets when secret values debug enabled
-		DebugPrintValues(ctx, "all", vals)
-	}
 
 	return vals, nil
 }

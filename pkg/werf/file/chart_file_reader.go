cd62d6e10455efa621e833c323a6ecacdf56e190
Ilya Lesikov <mail@lesikov.com>
2025-06-25 12:18:10 +0300

    refactor: use ChartFileReader for os.Stat (#374)
    refactor: use ChartFileReader for os.Stat

--- pkg/werf/file/chart_file_reader.go
+++ pkg/werf/file/chart_file_reader.go
@@ -8,6 +8,7 @@ type ChartFileReaderInterface interface {
 	LocateChart(ctx context.Context, name string) (string, error)
 	ReadChartFile(ctx context.Context, filePath string) ([]byte, error)
 	LoadChartDir(ctx context.Context, dir string) ([]*ChartExtenderBufferedFile, error)
+	ChartIsDir(relPath string) (bool, error)
 }
 
 // FIXME(ilya-lesikov): keep it global, but separate package? Make non-giterminism default implementation
